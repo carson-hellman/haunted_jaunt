bac2d53a016ab05374f78fe9d855a928a1cb0bcb
jhmac <jhmac121101@gmail.com>
2023-04-18 12:08:27 -0700

    added camera and game end

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Observer.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Observer.cs
@@ -10,23 +10,23 @@ public class Observer : MonoBehaviour
     bool m_IsPlayerInRange;
 
     void OnTriggerEnter (Collider other)
-    {
+    {/*
         if(other.transform == player)
         {
             m_IsPlayerInRange = true;
-        }
+        }*/
     }
 
     void OnTriggerExit (Collider other)
-    {
+    {/*
         if(other.transform == player)
         {
             m_IsPlayerInRange = false;
-        }
+        }*/
     }
 
     void Update()
-    {
+    {/*
         if(m_IsPlayerInRange)
         {
             Vector3 direction = player.position - transform.position + Vector3.up;
@@ -41,7 +41,7 @@ public class Observer : MonoBehaviour
                 }
             }
             
-        }
+        }*/
 
     }
 }
